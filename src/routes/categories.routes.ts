b251dcb532169e54fe20b102c736ed0e4e91d4d5
Sérgio <71034184+serjolas1@users.noreply.github.com>
2021-08-19 22:21:01 -0300

    id and created_at field on category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,23 @@
 import { Router } from "express";
+import { v4 as uuid } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (req, res) => {
+categoriesRoutes.post("/", (req, res) => {
     const { name, description } = req.body;
 
-    categories.push({
+    const category = {
+        id: uuid(),
         name,
         description,
-    });
+        created_at: new Date(),
+    };
 
-    return res.status(201).send();
+    categories.push(category);
+
+    return res.status(201).json(category);
 });
 
 export { categoriesRoutes };
