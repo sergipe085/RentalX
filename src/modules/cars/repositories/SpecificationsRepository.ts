5fa387b2dbec2ae85a3fe446e0efe325c96eafd3
Sérgio <71034184+serjolas1@users.noreply.github.com>
2021-08-23 22:09:15 -0300

    specification repository & create specification service

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,28 @@
+import { Specification } from "../models/Specification";
+import {
+    ICreateSpecificationDTO,
+    ISpecificationsRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificiationsRepository implements ISpecificationsRepository {
+    private specifications: Specification[];
+
+    constructor() {
+        this.specifications = [];
+    }
+
+    create({ name, description }: ICreateSpecificationDTO): void {
+        const specification = new Specification(name, description, new Date());
+        this.specifications.push(specification);
+    }
+
+    findByName(name: string): Specification {
+        const specification = this.specifications.find(
+            (value) => value.name === name
+        );
+
+        return specification;
+    }
+}
+
+export { SpecificiationsRepository };
--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,22 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
 class CreateSpecificationService {
-    execute() {
-        console.log("todo");
+    constructor(private specificationsRepository: ISpecificationsRepository) {}
+
+    execute({ name, description }: IRequest): void {
+        const specificationAlreadyExists =
+            this.specificationsRepository.findByName(name);
+
+        if (specificationAlreadyExists) {
+            throw new Error("This specification already exists");
+        }
+
+        this.specificationsRepository.create({ name, description });
     }
 }
 
