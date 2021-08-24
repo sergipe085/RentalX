640a34bfcfb75767e6aa1ffea36f6053dd6de16f
Sérgio <71034184+serjolas1@users.noreply.github.com>
2021-08-23 22:19:16 -0300

    specifications routes & create specification route

---
+++ src/routes/specifications.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+import { SpecificiationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationsRoutes = Router();
+const specificationsRepository = new SpecificiationsRepository();
+
+specificationsRoutes.post("/", (req, res) => {
+    const { name, description } = req.body;
+
+    const createSpecificationService = new CreateSpecificationService(
+        specificationsRepository
+    );
+
+    createSpecificationService.execute({ name, description });
+
+    return res.status(201).send();
+});
+
+export { specificationsRoutes };
