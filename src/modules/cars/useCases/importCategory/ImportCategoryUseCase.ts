f67f69528d0025496e1d4328fe8456c5ad38bd02
Sérgio <71034184+serjolas1@users.noreply.github.com>
2021-09-24 23:27:28 -0300

    dependency injection with tstryng

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,6 @@
 import csvParse from "csv-parse";
 import fs from "fs";
+import { inject, injectable } from "tsyringe";
 
 import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
 
@@ -8,8 +9,12 @@ interface IImportCategory {
     description: string;
 }
 
+@injectable()
 class ImportCategoryUseCase {
-    constructor(private categoriesRepository: CategoriesRepository) {}
+    constructor(
+        @inject("CategoriesRepository")
+        private categoriesRepository: CategoriesRepository
+    ) {}
 
     loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
         return new Promise((resolve, reject) => {
@@ -43,10 +48,12 @@ class ImportCategoryUseCase {
         categories.map(async (value) => {
             const { name, description } = value;
 
-            const categoryExists = this.categoriesRepository.findByName(name);
+            const categoryExists = await this.categoriesRepository.findByName(
+                name
+            );
 
             if (!categoryExists) {
-                this.categoriesRepository.create({
+                await this.categoriesRepository.create({
                     name,
                     description,
                 });
