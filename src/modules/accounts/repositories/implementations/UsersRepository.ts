e6df6fd54c04ff8a4f5aa8dfd081ab34cc114693
Sérgio <71034184+serjolas1@users.noreply.github.com>
2022-03-29 22:20:11 -0300

    user create useCase and routes

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -1,7 +1,7 @@
 import { getRepository, Repository } from "typeorm";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
-import { User } from "../../entitites/User";
+import { User } from "../../entities/User";
 import { IUsersRepository } from "../IUsersRepository";
 
 class UsersRepository implements IUsersRepository {
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
+import { userRoutes } from "./users.routes";
 
 const routes = Router();
 
 routes.use("/categories", categoriesRoutes);
 routes.use("/specifications", specificationsRoutes);
+routes.use("/users", userRoutes);
 
 export default routes;
